d2d5054b348ed9f0e74981d0ad00f63b0000ad98
lemeneguel <lecomp@icloud.com>
2025-11-12 16:32:25 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Leandro Meneguel on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by Leandro Meneguel on 11/12/25.
+//
+
+import Testing
+@testable import Todo
+
+struct TodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
